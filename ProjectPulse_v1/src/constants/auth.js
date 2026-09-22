fd25f464a0322b321a608f0975f9c7c97a702dd4
premenoch13 <premenoch13@gmail.com>
2026-09-23 00:12:31 +0530

    Delete ProjectPulse_v1 directory

--- ProjectPulse_v1/src/constants/auth.js
+++
@@ -1,3 +0,0 @@
-export const DEMO_USERNAME = "ProjectPulse";
-
-export const DEMO_PASSWORD = "Devoir@123";
--- ProjectPulse_v1/src/utils/format.js
+++
@@ -1,7 +0,0 @@
-export function parseCurrency(str) {
-  return Number((str || "").replace(/[^0-9]/g, "")) || 0;
-}
-
-export function formatINR(n) {
-  return "₹" + Number(n).toLocaleString("en-IN");
-}
